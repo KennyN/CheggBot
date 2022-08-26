0e01aa239a8867ec1227005f37381e51cbf9fb7d
Kenny <32527059+KennyN@users.noreply.github.com>
2022-08-25 20:38:46 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+{   
+    "info": {
+        "name": "Luna Tools",
+        "version": "0.0.1",
+        "proxy": {
+            "server": "REDACTED",
+            "username": "REDACTED",
+            "password": "REDACTED"
+        }
+    },
+    "prefix": "!",
+    "token": "REDACTED",
+    "words_array": ["Hi", "Bye", "Howdy"]
+}
